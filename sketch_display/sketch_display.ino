ca630d31627a904a61f2e7fb694344f9306054e1
Stéphane Bressani <stephane@bressani.dev>
2024-09-14 00:13:35 +0200

    les sprites s'affichent correctement

--- sketch_display/sketch_display.ino
+++ sketch_display/sketch_display.ino
@@ -162,10 +162,55 @@ const uint8_t spriteE[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
   {1, 1, 1},
 };
 
-// Tableau de correspondance entre les caractères et les sprites
-const uint8_t* sprites[] = {sprite0[0], sprite1[0], sprite2[0], sprite3[0], sprite4[0], sprite5[0], sprite6[0], sprite7[0], sprite8[0], sprite9[0], spriteB[0], spriteA[0], spriteN[0], spriteK[0], spriteP[0], spriteT[0], spriteE[0]};
+const uint8_t spriteR[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
+  {1, 1, 1},
+  {1, 0, 1},
+  {1, 1, 1},
+  {1, 1, 0},
+  {1, 0, 1},
+  {1, 0, 1},
+};
 
-const char spriteChars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'A', 'N', 'K', 'P', 'T', 'E'};
+const uint8_t spriteS[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
+  {1, 1, 1},
+  {1, 0, 0},
+  {1, 1, 1},
+  {0, 0, 1},
+  {0, 0, 1},
+  {1, 1, 1},
+};
+
+const uint8_t spriteI[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
+  {1, 1, 1},
+  {0, 1, 0},
+  {0, 1, 0},
+  {0, 1, 0},
+  {0, 1, 0},
+  {1, 1, 1},
+};
+
+const uint8_t spriteZ[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
+  {1, 1, 1},
+  {0, 0, 1},
+  {0, 1, 0},
+  {0, 1, 0},
+  {1, 0, 0},
+  {1, 1, 1},
+};
+
+const uint8_t spriteSlash[SPRITE_HEIGHT][SPRITE_WIDTH] PROGMEM = {
+  {0, 0, 1},
+  {0, 0, 1},
+  {0, 1, 0},
+  {0, 1, 0},
+  {1, 0, 0},
+  {1, 0, 0},
+};
+
+// Tableau de correspondance entre les caractères et les sprites
+const uint8_t* sprites[] = {sprite0[0], sprite1[0], sprite2[0], sprite3[0], sprite4[0], sprite5[0], sprite6[0], sprite7[0], sprite8[0], sprite9[0], spriteB[0], spriteA[0], spriteN[0], spriteK[0], spriteP[0], spriteT[0], spriteE[0], spriteR[0], spriteS[0], spriteI[0], spriteZ[0], spriteSlash[0]};
+
+const char spriteChars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'A', 'N', 'K', 'P', 'T', 'E', 'R', 'S', 'I', 'Z', '/'};
 
 
 const uint8_t pixelData[64][128] PROGMEM = {
@@ -173,6 +218,29 @@ const uint8_t pixelData[64][128] PROGMEM = {
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   // Ajoute les autres lignes ici
 };
+
+void displaySprite(uint8_t position, char spriteChar, uint8_t offset = 0) {
+  const uint8_t* selectedSprite = nullptr;
+
+  // Trouver le sprite correspondant au caractère
+  for (uint8_t i = 0; i < sizeof(spriteChars); i++) {
+    if (spriteChars[i] == spriteChar) {
+      selectedSprite = sprites[i];
+      break;
+    }
+  }
+
+  // Si le sprite est trouvé, afficher
+  if (selectedSprite) {
+    for (uint8_t y = 0; y < SPRITE_HEIGHT; y++) {
+      for (uint8_t x = 0; x < SPRITE_WIDTH; x++) {
+        if (pgm_read_byte(&(selectedSprite[y * SPRITE_WIDTH + x])) == 1) {
+          display.drawPixel(offset + x + 1 + (position * (SPRITE_WIDTH + 2)), y + SCREEN_HEIGHT - SPRITE_HEIGHT, SSD1306_WHITE);
+        }
+      }
+    }
+  }
+}
 
 void setup() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -215,6 +283,12 @@ void setup() {
   displaySprite(16, '0');
   displaySprite(17, '0');
   displaySprite(18, '1');
+  displaySprite(19, ' ');
+  displaySprite(20, '1', 3);
+  displaySprite(21, '6', 3);
+  displaySprite(22, '/', 3);
+  displaySprite(23, '1', 3);
+  displaySprite(24, '6', 3);
 
   // Dessiner chaque pixel de la matrice
   for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
@@ -227,29 +301,6 @@ void setup() {
 
 
   display.display(); // Affiche tout
-}
-
-void displaySprite(uint8_t position, char spriteChar) {
-  const uint8_t* selectedSprite = nullptr;
-
-  // Trouver le sprite correspondant au caractère
-  for (uint8_t i = 0; i < sizeof(spriteChars); i++) {
-    if (spriteChars[i] == spriteChar) {
-      selectedSprite = sprites[i];
-      break;
-    }
-  }
-
-  // Si le sprite est trouvé, afficher
-  if (selectedSprite) {
-    for (uint8_t y = 0; y < SPRITE_HEIGHT; y++) {
-      for (uint8_t x = 0; x < SPRITE_WIDTH; x++) {
-        if (pgm_read_byte(&(selectedSprite[y * SPRITE_WIDTH + x])) == 1) {
-          display.drawPixel(x + 1 + (position * (SPRITE_WIDTH + 2)), y + SCREEN_HEIGHT - SPRITE_HEIGHT, SSD1306_WHITE);
-        }
-      }
-    }
-  }
 }
 
 void loop() {
